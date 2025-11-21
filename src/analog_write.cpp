57839bc1c6834a7b488aeaef2a9c919d145ec6fe
ghost-mann <iamaob8@protonmail.com>
2025-11-21 17:38:17 +0300

    feat: using button and serial monitor to register digital read in digital_read.cpp

--- src/analog_write.cpp
+++ src/analog_write.cpp
@@ -1,28 +1,28 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-const int ledPin = 9;
+// const int ledPin = 9;
 
-void setup() {
+// void setup() {
 
-}
+// }
 
-void loop() {
-    for (int value = 0; value <= 255; value += 5) {
-        analogWrite(ledPin, value);
-        delay(50);
-    }
+// void loop() {
+//     for (int value = 0; value <= 255; value += 5) {
+//         analogWrite(ledPin, value);
+//         delay(50);
+//     }
 
-    for (int value = 255; value >= 0; value -= 5){
-        analogWrite(ledPin, value);
-        delay(50);
-    }
+//     for (int value = 255; value >= 0; value -= 5){
+//         analogWrite(ledPin, value);
+//         delay(50);
+//     }
 
-    analogWrite(9,0);
-    delay(1000);
-    analogWrite(9,64);
-    delay(1000);
-    analogWrite(9,128);
-    delay(1000);
-    analogWrite(9,255);
-    delay(1000);
-}
+//     analogWrite(9,0);
+//     delay(1000);
+//     analogWrite(9,64);
+//     delay(1000);
+//     analogWrite(9,128);
+//     delay(1000);
+//     analogWrite(9,255);
+//     delay(1000);
+// }
---
+++ src/digital_read.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(2, INPUT);
+}
+
+void loop() {
+    int buttonState = digitalRead(2);
+    Serial.println(buttonState);
+    delay(1);
+}
