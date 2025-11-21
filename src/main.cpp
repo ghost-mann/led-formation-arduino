394df017c1cc29a1740d3d7e3c9130e11b4aadb3
ghost-mann <iamaob8@protonmail.com>
2025-11-21 11:53:42 +0300

    feat: cpp code programming an led to blink in led.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-void setup() {
-  for(int i=2;i<=11;i++)
-  {
-    pinMode(i,OUTPUT);
-  }
-}
+// void setup() {
+//   for(int i=2;i<=11;i++)
+//   {
+//     pinMode(i,OUTPUT);
+//   }
+// }
 
-void loop() {
-  for(int i=2;i<=11;i++)
-  {
-    digitalWrite(i,HIGH);
-    delay(400);
-    digitalWrite(i,LOW);
-    delay(500);    
-  }
-}
+// void loop() {
+//   for(int i=2;i<=11;i++)
+//   {
+//     digitalWrite(i,HIGH);
+//     delay(400);
+//     digitalWrite(i,LOW);
+//     delay(500);    
+//   }
+// }
