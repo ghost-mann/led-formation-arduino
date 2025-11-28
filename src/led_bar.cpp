5da36c3aa655cd4d77c635391387532e47fffe2e
ghost-mann <iamaob8@protonmail.com>
2025-11-28 13:44:42 +0300

    feat: added blink sketch in led_bar.cpp

---
+++ src/led_bar.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+int ledPin[4] = {13,12,11,10};
+
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPin[i], OUTPUT);
+  }
+
+}
+
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledPin[i],HIGH);
+  }
+  delay(1000);
+  
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(ledPin[i],LOW);
+  }
+
+  delay(1000);
+
+}
